1ce8af0870cb72987fa69024e3a8a07e3dad2d21
iman92dez <iman.barghandan1371@gmail.com>
2020-10-15 19:36:54 +0330

    start MainMenu.java

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,8 +1,10 @@
+import controller.MainMenu;
 import services.AccountService;
 
 public class MainApp {
     public static void main(String[] args) {
-        AccountService accountService = new AccountService();
-        accountService.signUp("iman","brg","u4","p4","30");
+        MainMenu.showMenu();
+//        AccountService accountService = new AccountService();
+//        accountService.signUp("iman","brg","u4","p4","30");
     }
 }
---
+++ src/main/java/controller/MainMenu.java
@@ -0,0 +1,25 @@
+package controller;
+
+import scanner.ScannerClass;
+
+public class MainMenu {
+    public static void showMenu() {
+        int selectedNumber;
+        while (true) {
+            System.out.println("(1) Sign-In\n" +
+                    "(2) Sign-Up\n" +
+                    "(3) End program");
+            selectedNumber = ScannerClass.getNumber(1, 3);
+
+            if (selectedNumber == 1)
+            {
+
+            } else if (selectedNumber == 2)
+            {
+
+            } else{
+                break;
+            }
+        }
+    }
+}
